6e56eafbcc42b381268d384b1daf69e89c7e2d4d
John Hoestje <john.hoestje+GITHUB@gmail.com>
2020-05-19 23:03:47 -0500

    Project Skeleton
    initial project skeleton commit

---
+++ repository/src/main/java/org/johoco/depinsight/repository/App.java
@@ -0,0 +1,13 @@
+package org.johoco.depinsight.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
