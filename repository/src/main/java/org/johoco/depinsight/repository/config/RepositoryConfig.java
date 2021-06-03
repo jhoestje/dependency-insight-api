88c9b2285e2f04fc8499946136495f61f970e2f4
John Hoestje <john.hoestje+GITHUB@gmail.com>
2021-06-02 20:26:41 -0500

    Update RepositoryConfig.java
    Adding database creation and starting graph creation

--- repository/src/main/java/org/johoco/depinsight/repository/config/RepositoryConfig.java
+++ repository/src/main/java/org/johoco/depinsight/repository/config/RepositoryConfig.java
@@ -1,5 +1,8 @@
 package org.johoco.depinsight.repository.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.johoco.depinsight.domain.GroupId;
 import org.johoco.depinsight.domain.Language;
 import org.johoco.depinsight.domain.relationship.LanguageType;
@@ -8,6 +11,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.ArangoDatabase;
+import com.arangodb.entity.EdgeDefinition;
+import com.arangodb.entity.GraphEntity;
 import com.arangodb.springframework.annotation.EnableArangoRepositories;
 import com.arangodb.springframework.config.ArangoConfiguration;
 import com.arangodb.springframework.core.ArangoOperations;
@@ -40,12 +47,49 @@ public class RepositoryConfig implements ArangoConfiguration {
 	@Override
 	@Bean
 	public ArangoOperations arangoTemplate() throws Exception {
-		ArangoOperations operations = new ArangoTemplate(arango().build(), database(), arangoConverter(),
-				resolverFactory());
+		// ArangoDBImpl
+		ArangoDB arangoDB = arango().build();
+
+		ArangoOperations operations = new ArangoTemplate(arangoDB, database(), arangoConverter(), resolverFactory());
+		ArangoDatabase arangoDatabase = arangoDB.db(database());
+
+		createDatabase(arangoDatabase);
+
+//		CollectionEntity myArangoCollection = arangoDatabase.createCollection("collectionName");
+
+		final Collection<EdgeDefinition> edgeDefinitions = new ArrayList<>();
+		final EdgeDefinition edgeDefinition = new EdgeDefinition().collection("EDGE_COLLECTION_NAME")
+				.from("VERTEXT_COLLECTION_NAME").to("VERTEXT_COLLECTION_NAME");
+		edgeDefinitions.add(edgeDefinition);
+		arangoDatabase.createGraph("GRAPH_NAME", edgeDefinitions, null);
+
+		// check if graph is created
+		Collection<GraphEntity> graphs = arangoDatabase.getGraphs();
+		graphs.isEmpty();
 
 		setupDatabase(operations);
 
 		return operations;
+	}
+
+	private void createDatabase(final ArangoDatabase arangoDatabase) {
+		Boolean databaseExists = arangoDatabase.exists();
+		System.out.println("=== Does ArangoDB Database exist:  " + databaseExists.toString());
+
+		if (Boolean.FALSE.equals(databaseExists)) {
+			System.out.println("=== ArangoDB Database does not exist");
+			try {
+
+				if (arangoDatabase.create()) {
+					System.out.println("=== Created ArangoDB Database");
+				} else {
+					System.out.println("===!!!! Did not create ArangoDB Database");
+				}
+			} catch (ArangoDBException adbe) {
+				System.out.println("===!!!! ArangoDBException ArangoDBException ArangoDBException ArangoDBException "
+						+ adbe.getErrorMessage());
+			}
+		}
 	}
 
 	/**
@@ -54,6 +98,7 @@ public class RepositoryConfig implements ArangoConfiguration {
 	 * @param operations
 	 */
 	public void setupDatabase(final ArangoOperations operations) {
+
 		operations.collection(Language.class);
 		operations.collection(LanguageType.class);
 		operations.collection(GroupId.class);
