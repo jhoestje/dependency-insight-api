790641e9874320e626dcd363eb380f00b63e344e
John Hoestje <john.hoestje+GITHUB@gmail.com>
2021-07-01 19:54:54 -0500

    api excpetions

---
+++ web/src/main/java/org/johoco/depinsight/web/exception/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package org.johoco.depinsight.web.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+//	EmployeeNotFoundException(Long id) {
+//	    super("Could not find employee " + id);
+//	  }
+}
